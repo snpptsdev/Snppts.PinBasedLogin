ceddc319382228856165712d1d246a643f33b1fb
snppts <sthewissen@Stevens-MacBook-Pro-2.local>
2016-02-16 21:29:14 +0100

    Custom font for Android

---
+++ Droid/CustomRenderers/CustomLabelRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Graphics;
+using Snppts.PinBasedLogin.Droid;
+
+[assembly:ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace Snppts.PinBasedLogin.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (!string.IsNullOrEmpty(e.NewElement?.FontFamily))
+            {
+                try
+                {
+                    var font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, e.NewElement.FontFamily + ".ttf");
+
+                    if (font != null)
+                    {
+                        Control.Typeface = font;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Ignore it, means we couldn't find the asset.
+                }
+            }
+        }
+    }
+}
+
+
